4623684cd6c7ae27268186ec5d8b5ff7087f0d38
mertbayca <mertbayca@gmail.com>
2022-12-12 23:12:38 +0300

    Fixed word in article

--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -14,7 +14,7 @@ const blog = () => {
           <Article imgUrl={blog01} date="Dec 15, 2022" title="Now accepting beta testers for ScoPym Mobile. Here's how you can apply: Dev Journal Week 38"/>
         </div>
         <div className="scop__blog-container_groupB">
-          <Article imgUrl={blog02} date="Dec 10, 2022" title="AI and web3: How will scop affect the cryptospace?"/>
+          <Article imgUrl={blog02} date="Dec 10, 2022" title="AI and web3: How will GPT3 affect the cryptospace?"/>
           <Article imgUrl={blog03} date="Dec 09, 2022" title="ScoPym now partnered with Binance!"/>
           <Article imgUrl={blog04} date="Dec 08, 2022" title="Development on ScoPym Mobile near completion: Dev Journal Week 37"/>
           <Article imgUrl={blog05} date="Dec 07, 2022" title="How are we taking precautions against hackers? An in-depth answer to an important topic"/>
