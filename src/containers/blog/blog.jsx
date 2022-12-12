dca9caabcb2f648c0b94a8a63d49b0d2977ee71b
mertbayca <mertbayca@gmail.com>
2022-12-12 23:07:23 +0300

    Did some polishing, removed placeholder tags

--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -5,16 +5,16 @@ import {blog01, blog02, blog03, blog04, blog05} from "./imports"
 
 const blog = () => {
   return (
-    <div className="gpt3__blog section__padding">
-      <div className="gpt3__blog-heading">
+    <div className="scop__blog section__padding">
+      <div className="scop__blog-heading">
         <h1 className="gradient__text">This is a fast changing space. Check out how we're keeping up.</h1>
       </div>
-      <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container_groupA">
+      <div className="scop__blog-container">
+        <div className="scop__blog-container_groupA">
           <Article imgUrl={blog01} date="Dec 15, 2022" title="Now accepting beta testers for ScoPym Mobile. Here's how you can apply: Dev Journal Week 38"/>
         </div>
-        <div className="gpt3__blog-container_groupB">
-          <Article imgUrl={blog02} date="Dec 10, 2022" title="AI and web3: How will GPT3 affect the cryptospace?"/>
+        <div className="scop__blog-container_groupB">
+          <Article imgUrl={blog02} date="Dec 10, 2022" title="AI and web3: How will scop affect the cryptospace?"/>
           <Article imgUrl={blog03} date="Dec 09, 2022" title="ScoPym now partnered with Binance!"/>
           <Article imgUrl={blog04} date="Dec 08, 2022" title="Development on ScoPym Mobile near completion: Dev Journal Week 37"/>
           <Article imgUrl={blog05} date="Dec 07, 2022" title="How are we taking precautions against hackers? An in-depth answer to an important topic"/>
--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -4,32 +4,32 @@ import flogo from "../../assets/logo.svg"
 
 const footer = () => {
   return (
-    <div className="gpt3__footer section__padding">
-      <div className="gpt3__footer-heading">
+    <div className="scop__footer section__padding">
+      <div className="scop__footer-heading">
         <h1 className="gradient__text">Step into the future and become the leaders of tomorrow.</h1>
       </div>
-      <div className="gpt3__footer-links">
-        <div className="gpt3__footer-links_logo">
+      <div className="scop__footer-links">
+        <div className="scop__footer-links_logo">
           <img src={flogo} alt="logo"/>
           <p>Website created by Mert Bayça. All rights reserved</p>
         </div>
-        <div className="gpt3__footer-links_div">
+        <div className="scop__footer-links_div">
           <h4>Links</h4>
           <p>Social Media</p>
           <p>Contact</p>
         </div>
-        <div className="gpt3__footer-links_div">
+        <div className="scop__footer-links_div">
           <h4>Company</h4>
           <p>Terms & Conditions</p>
           <p>Privacy Policy</p>
         </div>
-        <div className="gpt3__footer-links_div">
+        <div className="scop__footer-links_div">
           <h4>Get in touch</h4>
           <p>mertbayca@gmail.com</p>
           <p>Placeholder St. P34 C32 Somewhere/Allthetime</p>
         </div>
       </div>
-      <div className="gpt3__footer-copyright">
+      <div className="scop__footer-copyright">
         <p>ScoPym 2022. All rights reserved</p>
       </div>
     </div>
