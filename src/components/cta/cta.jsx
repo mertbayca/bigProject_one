c007589f18eeb189c39ca998735dd0ea850dcbdd
mertbayca <mertbayca@gmail.com>
2022-12-12 21:53:04 +0300

    cta done

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -3,7 +3,15 @@ import "./cta.css"
 
 const cta = () => {
   return (
-    <div>cta</div>
+    <div className="scop__cta">
+      <div className="scop__cta-content">
+        <p>Request Info Now</p>
+        <h3>Register today & start exploring the possibilities of web3.</h3>
+      </div>
+      <div className="scop__cta-button">
+        <button type="button" onClick={() => {window.open("header")}}>Get Started</button>
+      </div>
+    </div>
   )
 }
 
